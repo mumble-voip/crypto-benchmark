56d925b5953273f388435f4ab8736895a3fe8323
Davide Beatrici <git@davidebeatrici.dev>
2020-05-04 20:58:52 +0200

    Add CMake options to toggle individual benchmarks

--- main.c
+++ main.c
@@ -10,25 +10,31 @@
 #include <stdlib.h>
 
 int main() {
+#if BENCHMARK_HYDROGEN
 	if (!hydrogen_main(MESSAGE_SIZE, ITERATIONS)) {
 		return EXIT_FAILURE;
 	}
-
+#endif
+#if BENCHMARK_NSS
 	if (!nss_main(MESSAGE_SIZE, ITERATIONS)) {
 		return EXIT_FAILURE;
 	}
-
+#endif
+#if BENCHMARK_OPENSSL
 	if (!openssl_main(MESSAGE_SIZE, ITERATIONS)) {
 		return EXIT_FAILURE;
 	}
-
+#endif
+#if BENCHMARK_SODIUM
 	if (!sodium_main(MESSAGE_SIZE, ITERATIONS)) {
 		return EXIT_FAILURE;
 	}
-
+#endif
+#if BENCHMARK_WOLFCRYPT
 	if (!wolfcrypt_main(MESSAGE_SIZE, ITERATIONS)) {
 		return EXIT_FAILURE;
 	}
+#endif
 
 	return EXIT_SUCCESS;
 }
